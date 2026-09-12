cfd28dff18b193fad6abd721ca1bcf1878053118
sybah399-byte <sybah399@gmail.com>
2026-09-12 12:28:53 +0930

    Register persistent project IPC

--- electron/main.js
+++ electron/main.js
@@ -26,31 +26,21 @@ const {
 const registerCreateProjectIPC =
     require("./ipc/createProject");
 
+const registerProjectsIPC =
+    require("./ipc/projects");
+
 function createWindow() {
 
     const win = new BrowserWindow({
-
         width: 1400,
-
         height: 900,
-
         minWidth: 1100,
-
         minHeight: 700,
-
         webPreferences: {
-
-            preload: path.join(
-                __dirname,
-                "preload.js"
-            ),
-
+            preload: path.join(__dirname, "preload.js"),
             contextIsolation: true,
-
             nodeIntegration: false
-
         }
-
     });
 
     const viteURL =
@@ -58,198 +48,94 @@ function createWindow() {
         "http://localhost:5173";
 
     win.loadURL(viteURL);
-
 }
-
-/*
-=========================================
-Terminal
-=========================================
-*/
 
 ipcMain.handle(
     "run-command",
-    async (event, command) => {
-
-        return await runCommand(command);
-
-    }
+    async (event, command) => await runCommand(command)
 );
-
-/*
-=========================================
-Project Scanner
-=========================================
-*/
 
 ipcMain.handle(
     "scan-project",
-    async (event, folder) => {
-
-        return await scanProject(folder);
-
-    }
+    async (event, folder) => await scanProject(folder)
 );
-
-/*
-=========================================
-Workspace
-=========================================
-*/
 
 ipcMain.handle(
     "open-folder",
-    async (event, folder) => {
-
-        return await openFolder(folder);
-
-    }
+    async (event, folder) => await openFolder(folder)
 );
 
 ipcMain.handle(
     "open-vscode",
-    async (event, folder) => {
-
-        return await openVSCode(folder);
-
-    }
+    async (event, folder) => await openVSCode(folder)
 );
 
 ipcMain.handle(
     "open-terminal",
-    async (event, folder) => {
-
-        return await openTerminal(folder);
-
-    }
+    async (event, folder) => await openTerminal(folder)
 );
 
 ipcMain.handle(
     "run-expo",
-    async (event, folder) => {
-
-        return await runExpo(folder);
-
-    }
+    async (event, folder) => await runExpo(folder)
 );
-
-/*
-=========================================
-Git
-=========================================
-*/
 
 ipcMain.handle(
     "git-status",
-    async (event, folder) => {
-
-        return await gitStatus(folder);
-
-    }
+    async (event, folder) => await gitStatus(folder)
 );
 
 ipcMain.handle(
     "git-branch",
-    async (event, folder) => {
-
-        return await gitBranch(folder);
-
-    }
+    async (event, folder) => await gitBranch(folder)
 );
 
 ipcMain.handle(
     "git-init",
-    async (event, folder) => {
-
-        return await gitInit(folder);
-
-    }
+    async (event, folder) => await gitInit(folder)
 );
 
 ipcMain.handle(
     "git-add",
-    async (event, folder) => {
-
-        return await gitAdd(folder);
-
-    }
+    async (event, folder) => await gitAdd(folder)
 );
 
 ipcMain.handle(
     "git-commit",
-    async (event, data) => {
-
-        return await gitCommit(
-            data.folder,
-            data.message
-        );
-
-    }
+    async (event, data) =>
+        await gitCommit(data.folder, data.message)
 );
 
 ipcMain.handle(
     "git-pull",
-    async (event, folder) => {
-
-        return await gitPull(folder);
-
-    }
+    async (event, folder) => await gitPull(folder)
 );
 
 ipcMain.handle(
     "git-push",
-    async (event, folder) => {
-
-        return await gitPush(folder);
-
-    }
+    async (event, folder) => await gitPush(folder)
 );
 
 ipcMain.handle(
     "git-fetch",
-    async (event, folder) => {
-
-        return await gitFetch(folder);
-
-    }
+    async (event, folder) => await gitFetch(folder)
 );
 
-/*
-=========================================
-Project Creation
-=========================================
-*/
-
+registerProjectsIPC();
 registerCreateProjectIPC();
 
 app.whenReady().then(() => {
-
     createWindow();
-
 });
 
-app.on(
-    "window-all-closed",
-    () => {
-
-        if (process.platform !== "darwin") {
-
-            app.quit();
-
-        }
-
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
     }
-);
+});
 
-app.on(
-    "activate",
-    () => {
-
-        if (BrowserWindow.getAllWindows().length === 0) {
-
-            createWindow();
-
-        }
-
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
     }
-);
+});
